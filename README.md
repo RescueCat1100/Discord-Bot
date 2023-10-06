fc6038b20282f15b180e0d2ed7e61c6f1288453e
RescueCat1100 <1100.minh.tran@gmail.com>
2023-10-06 10:46:38 +0700

    Big update.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Updating...
+_v1.2.1_
+#By RescueCat1100 on GitHub | meimei1100 on Discord
+
+# General Discord Bot with Yu-Gi-Oh! OCG built-in function
+
+## How to run
+
+- Install Json Generator for generating data in correct json form
+- Install required packages `pip install -r requirements.txt`
+- Place json files in same directory as `bot.py`.
+- The template of those files are included in this repo. You can make your own data and modify the reading process at `/cogs/slash/card_related.py`
+- Edit `config.json` accordingly
+
+## How to use
+
+- Invite bot to server
+- Run slash command `cardinfo` to search by name or `cardcode` to search by set code
+
+## Known issues
+
+- User need to either include special characters or skip them with a space. It should not be a problem unless card with multiple continuous special characters like `Maxx "C"`
+- Delay time
+- Not matching real-time data could lead to potential misinformation. But caching own data will asure the bot work even when 3rd party down.
--- cogs/slash/card_related.py
+++ cogs/slash/card_related.py
@@ -7,187 +7,19 @@ from disnake import ApplicationCommandInteraction, Option, OptionType
 from disnake.ext import commands
 from helpers import checks
 from pathlib import Path
-
-async def search_engine(input_string, url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as r:
-            if r.status == 200:       
-                data = await r.json()
-                if "cards" in str(url):
-                    for card in data:
-                        for card_name in card:
-                            card_details = card[card_name]
-                            jp_name = card_details["Japanese name"]
-                            id = card_details["Card ID"]
-                            details = card_details["Details"]
-                        
-                            if re.sub('[^a-zA-Z0-9 \n\.]', ' ', input_string).lower() \
-                            == re.sub('[^a-zA-Z0-9 \n\.]', ' ', card_name).lower():
-                            
-                                embed = disnake.Embed(
-                                title=card_name,
-                                description=jp_name,
-                                timestamp=datetime.datetime.now(),
-                                color=0x9C84EF
-                                )
-
-                                for detail in details:                        
-                                    for set_code in detail:
-                                        
-                                        price = "Out stock!"
-                                        if detail[set_code]['price'] != 0:
-                                            price = "{} JPY".format(detail[set_code]['price'])
-                                        
-                                        rarity = detail[set_code]['rarity']
-                                        if rarity == "ｼｰｸﾚｯﾄ":
-                                            rarity = "SCR"
-                                        elif rarity == "【TRC1】ﾚｱﾘﾃｨ･ｺﾚｸｼｮﾝ":
-                                            rarity = "CR"
-                                        elif rarity == "ｱﾙﾃｨﾒｯﾄ":
-                                            rarity = "Ultimate R"
-                                        
-                                        condition = detail[set_code]['condition']
-                                        
-                                        embed.add_field(name="Info", 
-                                        value="{}\nPrice: {}\nRarity: {}\nCondition: {}" \
-                                        .format(set_code, price, rarity, condition),
-                                        inline=True)
-                                        
-                                id = int(id)
-                                path = "pics/{}.jpg".format(id)
-
-                                if Path(path).is_file():
-                                    pass
-                                else:
-                                    img_url = "https://images.ygoprodeck.com/images/cards/{}.jpg".format(id)
-                                    img_data = requests.get(img_url).content
-                                    with open(path, 'wb') as handler:
-                                        handler.write(img_data)
-
-                                embed.set_image(file=disnake.File(path))
-                                embed.set_footer(text="Card ID: {}".format(id),)
-                                return embed
+import json
 
 
-                    for card in data:
-                        for card_name in card:
-                            card_details = card[card_name]
-                            jp_name = card_details["Japanese name"]
-                            id = card_details["Card ID"]
-                            details = card_details["Details"]
-                            
-                            if re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(input_string)).lower() \
-                            in re.sub('[^a-zA-Z0-9 \n\.]', ' ', card_name).lower():
-                                
-                                embed = disnake.Embed(
-                                title=card_name,
-                                description=jp_name,
-                                timestamp=datetime.datetime.now(),
-                                color=0x9C84EF
-                                )
-
-                                for detail in details:                        
-                                    for set_code in detail:
-                                        
-                                        price = "Out stock!"
-                                        if detail[set_code]['price'] != 0:
-                                            price = "{} JPY".format(detail[set_code]['price'])
-                                        
-                                        rarity = detail[set_code]['rarity']
-                                        if rarity == "ｼｰｸﾚｯﾄ":
-                                            rarity = "SCR"
-                                        elif rarity == "【TRC1】ﾚｱﾘﾃｨ･ｺﾚｸｼｮﾝ":
-                                            rarity = "CR"
-                                        elif rarity == "ｱﾙﾃｨﾒｯﾄ":
-                                            rarity = "Ultimate R"
-                                        
-                                        condition = detail[set_code]['condition']
-                                        
-                                        embed.add_field(name="Info", 
-                                        value="{}\nPrice: {}\nRarity: {}\nCondition: {}" \
-                                        .format(set_code, price, rarity, condition),
-                                        inline=True)
-                                        
-                                id = int(id)
-                                path = "pics/{}.jpg".format(id)
-
-                                if Path(path).is_file():
-                                    pass
-                                else:
-                                    img_url = "https://images.ygoprodeck.com/images/cards/{}.jpg".format(id)
-                                    img_data = requests.get(img_url).content
-                                    with open(path, 'wb') as handler:
-                                        handler.write(img_data)
-
-                                embed.set_image(file=disnake.File(path))
-                                embed.set_footer(text="Card ID: {}".format(id),)
-                                return embed
-                elif "code" in str(url):
-                    #embed = disnake.Embed()
-                    for set_code in data:
-                        if re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(input_string)).lower() \
-                            in re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(set_code)).lower():
-                            embed = disnake.Embed(
-                            title=set_code,
-                            #description=jp_name,
-                            timestamp=datetime.datetime.now(),
-                            color=0x9C84EF
-                            )
-                            for code in data[set_code]:
-                                card_name = code["English name"]
-                                jp_name = code["Japanese name"]
-                                id = code["Card ID"]
-                                
-                                
-                                price = "Out stock!"
-                                if code['Details']['Price'] != 0:
-                                    price = "{} JPY".format(code['Details']['Price'])
-                                rarity = code['Details']['Rarity']
-                                if rarity == "ｼｰｸﾚｯﾄ":
-                                    rarity = "SCR"
-                                elif rarity == "【TRC1】ﾚｱﾘﾃｨ･ｺﾚｸｼｮﾝ":
-                                    rarity = "CR"
-                                elif rarity == "ｱﾙﾃｨﾒｯﾄ":
-                                    rarity = "Ultimate R"
-                                
-                                condition = code['Details']['Condition']
-                                embed.add_field(name="Info", 
-                                value="Price: {}\nRarity: {}\nCondition: {}" \
-                                .format(price, rarity, condition),
-                                inline=True)
-                                
-                            id = int(id)
-                            path = "pics/{}.jpg".format(id)
-
-                            if Path(path).is_file():
-                                pass
-                            else:
-                                img_url = "https://images.ygoprodeck.com/images/cards/{}.jpg".format(id)
-                                img_data = requests.get(img_url).content
-                                with open(path, 'wb') as handler:
-                                    handler.write(img_data)
-
-                            embed.set_image(file=disnake.File(path))
-                            embed.set_footer(text="Card ID: {}".format(id),)
-                            return embed
-                            #print(code)
-                            #print(set_code[code]["English name"])
-            else:
-                embed = disnake.Embed(
-                    title="Error!",
-                    description="There is something wrong with the server, please try again later. \
-                    Or contact me at MeiMei#3717 on Discord if the error still continue",
-                    color=0xE02B2B
-                )
-                return embed
 ###############################################################################
 ###############################################################################
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------#
 ######### Here we name the cog and create a new class for the cog.#############
+
+
 class card_realted(commands.Cog, name="card-slash"):
     def __init__(self, bot):
         self.bot = bot
-    
+
     # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
     @commands.slash_command(
         name="cardinfo",
@@ -195,35 +27,149 @@ class card_realted(commands.Cog, name="card-slash"):
     )
     @checks.not_blacklisted()
     async def card_name_search(self, interaction: ApplicationCommandInteraction, input_name: str):
+        
         await interaction.response.defer()
         with open("log.txt", "a") as log:
             log.write(input_name + "\n")
-        embed = disnake.Embed(
-            title="Error!",
-            description="Cannot find the card for now. \n Most likely our databases do not have the card.",
-            color=0xE02B2B
-        )
-        embed = await search_engine(input_string=input_name, url="http://localhost:8000/cards.json")
+        with open("cards.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+        found = False
+        
+        for cards in data:
+            for card in cards:
+                if re.sub('[^a-zA-Z0-9 \n\.]', ' ', input_name).lower() \
+                        == re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(card)).lower():
+                    card_details = cards[card]
+                    card_en_name = str(card)
+                    card_jp_name = str(card_details['Japanese name'])
+                    card_id = str(card_details['Card ID'])
+                    found = True
+                    break
+                elif re.sub('[^a-zA-Z0-9 \n\.]', ' ', input_name).lower() \
+                        in re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(card)).lower():
+                    card_details = cards[card]
+                    card_en_name = str(card)
+                    card_jp_name = str(card_details['Japanese name'])
+                    card_id = str(card_details['Card ID'])
+                    found = True
+            else:
+                continue
+            break
+        if (found):
+            embed = disnake.Embed(title=card_en_name,
+                                  description=card_jp_name,
+                                  timestamp=datetime.datetime.now(),
+                                  color=0x9C84EF
+                                  )
+            
+            for detail in card_details['Details']:
+                for set_code in detail:
+                    card_set_code = str(set_code)
+                    rarity = detail[set_code]['rarity']
+                    price = detail[set_code]['price']
+                    condition = detail[set_code]['condition']
+                    embed.add_field(name="Info",
+                                    value="{}\nPrice: {}\nRarity: {}\nCondition: {}"
+                                    .format(card_set_code, price, rarity, condition),
+                                    inline=True)
+            card_id = int(card_id)
+            path = "pics/{}.jpg".format(card_id)
+
+            if Path(path).is_file():
+                pass
+            else:
+                card_img_url = "https://images.ygoprodeck.com/images/cards/{}.jpg".format(card_id)
+                img_data = requests.get(card_img_url).content
+                with open(path, 'wb') as handler:
+                    handler.write(img_data)
+
+            embed.set_image(file=disnake.File(path))
+            embed.set_footer(text="Card ID: {}".format(card_id),)
+        else:
+            embed = disnake.Embed(
+                title="Error!",
+                description="Cannot find the card for now. \n Most likely our databases do not have the card.",
+                color=0xE02B2B
+            )
+
         await interaction.send(embed=embed)
 
+
+#############################################################
+#############################################################
     @commands.slash_command(
         name="cardcode",
         description="Input set code to find details about card. Contact MeiMei#3717 if things went wrong"
     )
     @checks.not_blacklisted()
     async def card_code_search(self, interaction: ApplicationCommandInteraction, input_code: str):
+        
         await interaction.response.defer()
         with open("log.txt", "a") as log:
             log.write(input_code + "\n")
+        with open("set_code.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+        found = False
+
+        for item in data:
+            for set_code in item:
+                if re.sub('[^a-zA-Z0-9 \n\.]', ' ', input_code).lower() \
+                        in re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(set_code)).lower():
+                    card_set_code = str(set_code)
+                    card_en_name = item[set_code]['English name']
+                    card_jp_name = item[set_code]['Japanese name']
+                    card_id = item[set_code]['Card ID']
+                    found = True
+                    break
+            else:
+                continue
+            break
+        if(found):
+            embed = disnake.Embed(
+                                    title=card_set_code,
+                                    description=card_en_name,
+                                    timestamp=datetime.datetime.now(),
+                                    color=0x9C84EF
+                                 )
+            with open("formatted_set_code.json", "r", encoding="utf-8") as f:
+                data = json.load(f)
+            for item in data:
+                if re.sub('[^a-zA-Z0-9 \n\.]', ' ', input_code).lower() \
+                        in re.sub('[^a-zA-Z0-9 \n\.]', ' ', str(item)).lower():
+                    for detail in data[item]:
+                        rarity = (detail['Rarity'])
+                        price = (detail['Price'])
+                        condition = (detail['Condition'])
+                    embed.add_field(name="Info",
+                        value="Price: {}\nRarity: {}\nCondition: {}"
+                        .format(price, rarity, condition),
+                        inline=True)
+
+            id = int(card_id)
+            path = "pics/{}.jpg".format(id)
+
+            if Path(path).is_file():
+                pass
+            else:
+                img_url = "https://images.ygoprodeck.com/images/cards/{}.jpg".format(id)
+                img_data = requests.get(img_url).content
+                with open(path, 'wb') as handler:
+                    handler.write(img_data)
+
+            embed.set_image(file=disnake.File(path))
+            embed.set_footer(text="Card ID: {}".format(id),)
         
-        embed = disnake.Embed(
-            title="Error!",
-            description="Cannot find the card for now. \n Most likely our databases do not have the card.",
-            color=0xE02B2B
-        )
-        embed = await search_engine(input_string=input_code, url="http://localhost:8000/formatted_set_code.json")
+        else:
+            embed = disnake.Embed(
+                title="Error!",
+                description="Cannot find the card for now. \n Most likely our databases do not have the card.",
+                color=0xE02B2B
+            )
+        
         await interaction.send(embed=embed)
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
+
+
 def setup(bot):
-    bot.add_cog(card_realted(bot))   
+    bot.add_cog(card_realted(bot))
